c48f7af8c51c9a75fd67bc843f15cdbd5ff1fae5
yaody123 <1415336788@qq.com>
2017-07-04 14:17:27 +0800

    Update WxPay.Config.php

--- lib/WxPay.Config.php
+++ lib/WxPay.Config.php
@@ -22,10 +22,10 @@ class WxPayConfig
 	 * 获取地址：https://mp.weixin.qq.com/advanced/advanced?action=dev&t=advanced/dev&token=2005451881&lang=zh_CN
 	 * @var string
 	 */
-	const APPID = 'wx426b3015555a46be';
-	const MCHID = '1900009851';
-	const KEY = '8934e7d15453e97507ef794cf7b0519d';
-	const APPSECRET = '7813490da6f1265e4901ffb80afaa36f';
+	const APPID = '微信APPID';
+	const MCHID = '商户ID号';
+	const KEY = '商户微信支付密钥';
+	const APPSECRET = '微信appsecret';
 	
 	//=======【证书路径设置】=====================================
 	/**
